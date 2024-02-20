87370cd75ac1fedd40fb907f2fd7e0c1e19dcc27
a-pak <kuivalainenap@gmail.com>
2024-02-20 19:00:21 +0200

    changes in deployment.py

--- warehouse/deployment.py
+++ warehouse/deployment.py
@@ -2,10 +2,25 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+DEBUG = False
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
-DEBUG = False
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFinesStorage'
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic"
+]
+
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
